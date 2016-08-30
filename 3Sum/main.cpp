837b36aeb3273f63ce372ff8189c4e2e63a1d51f
HoorayLee <leehorray@gmail.com>
2016-08-30 11:36:30 -0700

    Initial Commit

---
+++ 3Sum/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  3Sum
+//
+//  Created by 洪锐 李 on 16/8/30.
+//  Copyright © 2016年 洪锐 李. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
